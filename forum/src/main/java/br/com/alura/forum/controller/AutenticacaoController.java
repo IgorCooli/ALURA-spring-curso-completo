36056c2432764ae02849519a069662a2867410ec
igorcooli <igorcooli@gmail.com>
2021-08-05 15:57:12 -0300

    Projeto Final

---
+++ forum/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -0,0 +1,41 @@
+package br.com.alura.forum.controller;
+
+import br.com.alura.forum.config.security.TokenService;
+import br.com.alura.forum.controller.dto.TokenDto;
+import br.com.alura.forum.controller.form.LoginForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+@Profile("prod")
+public class AutenticacaoController {
+    @Autowired
+    AuthenticationManager authManager;
+
+    @Autowired
+    TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity<?> autenticar(@RequestBody @Valid LoginForm form){
+        UsernamePasswordAuthenticationToken dadosLogin = form.converter();
+        try {
+            Authentication auth = authManager.authenticate(dadosLogin);
+            String token = tokenService.gerarToken(auth);
+            return ResponseEntity.ok().body(new TokenDto(token, "Bearer"));
+        }catch (AuthenticationException e){
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
